70650cd6a83cba903a22f8d5d1ace0fc402f156a
Santhosh <pitakasanthosh@gmail.com>
2024-03-13 07:41:27 +0530

    Add npmUnInstall command to index.js

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ import welcome from "./utils/welcome.js";
 import { npmInit } from "./commands/init.js";
 import npmInstall from "./commands/depinstall.js";
 import npmInstallDevDep from "./commands/devDepInstall.js";
+import npmUnInstall from "./commands/uninstall.js";
 
 await welcome();
 await npmInit();
 await npmInstall();
 await npmInstallDevDep();
+await npmUnInstall();
