aba68c630a567431dbc3ef613f4c5fd48aada910
Santhosh <pitakasanthosh@gmail.com>
2024-03-13 07:40:46 +0530

    Add dev dependency installation command

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 import welcome from "./utils/welcome.js";
 import { npmInit } from "./commands/init.js";
 import npmInstall from "./commands/depinstall.js";
+import npmInstallDevDep from "./commands/devDepInstall.js";
 
 await welcome();
 await npmInit();
 await npmInstall();
+await npmInstallDevDep();
